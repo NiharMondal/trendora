8fd3d5950354bbcf02993346ee0c4b3fc2e33bd1
Nihar Mondal <developernihar@gmail.com>
2025-09-17 22:24:48 +0600

    removed: showcase section from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 import Footer from "@/components/common/footer";
 import HeroSection from "@/components/home/hero";
-import Showcase from "@/components/home/showcase";
-import Navbar from "@/components/layout/navbar";
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
 	return (
 		<main>
 			{/* <Navbar /> */}
 			<HeroSection />
-			<Showcase />
+
 			<Footer />
 		</main>
 	);
